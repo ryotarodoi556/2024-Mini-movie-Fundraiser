03182cf145b63d3e41677518526afaa81ecb6600
ryotarodoi556 <134979126+ryotarodoi556@users.noreply.github.com>
2024-06-05 09:34:23 +1200

    Add files via upload

---
+++ 00_FC_Base_v7.py
@@ -0,0 +1,34 @@
+# import libraries
+
+
+# *** functions go here ***
+
+# checks that input is either a float or an integer that is more than zero. takes in custom error message
+
+def num_check(question, error, num_type):
+    valid = False
+    while not valid:
+
+        try:
+            response = num_type(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+# Main routine goes here
+get_int = num_check("How many do you need? ",
+                    "Please enter an amount more than 0\n",
+                    int)
+get_cost = num_check("How much does it cost? $",
+                     "PLease enter a number more than 0\n",
+                     float)
+print("You need: {}".format(get_int))
+print("It costs: ${}".format(get_cost))
+
+
+
